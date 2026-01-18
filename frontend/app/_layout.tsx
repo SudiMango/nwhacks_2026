91daabb29dd47df748a2fd49164ca295c631d62a
Jon Liu <jonliu4242@gmail.com>
2026-01-17 22:13:37 -0800

    auth screen fix

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -8,7 +8,7 @@ import { BooksProvider } from '@/context/BooksContext';
 import { AuthProvider } from '@/context/AuthContext';
 
 export const unstable_settings = {
-  initialRouteName: '(tabs)',
+  initialRouteName: '(auth)',
 };
 
 export default function RootLayout() {
@@ -19,6 +19,7 @@ export default function RootLayout() {
       <BooksProvider>
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
           <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(auth)" />
             <Stack.Screen name="(onboarding)" />
             <Stack.Screen name="(tabs)" />
             <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
