898fb30a6a291a437a5d3ecb461c66a125c35c97
Jon Liu <jonliu4242@gmail.com>
2026-01-17 23:11:51 -0800

    chjage

--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -8,7 +8,7 @@ import React, {
 import * as SecureStore from "expo-secure-store";
 import { Alert } from "react-native";
 
-const BACKEND_URL = "http://10.19.134.189:8000";
+const BACKEND_URL = process.env.EXPO_PUBLIC_API_URL;
 const TOKEN_KEY = "auth_token";
 
 export interface UserProfile {
